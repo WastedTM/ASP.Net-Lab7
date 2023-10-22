12a7998b0cfb1ca8324e7778e3cb109a9a8f5e40
WastedTM <kilivnika62@gmail.com>
2023-10-22 18:05:54 +0300

    lab7

---
+++ lab7/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab7.Controllers;
+
+public class FileController : Controller
+{
+    // GET: /File/DownloadFile
+    public ActionResult DownloadFile()
+    {
+        return View();
+    }
+
+    // POST: /File/DownloadFile
+    [HttpPost]
+    public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+    {
+        if (!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName) && !string.IsNullOrWhiteSpace(fileName))
+        {
+            string content = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            byte[] fileContents = Encoding.UTF8.GetBytes(content);
+            string mimeType = "text/plain";
+
+            return File(fileContents, mimeType, fileName + ".txt");
+        }
+        return View();
+    }
+}
---
+++ lab7/Models/User.cs
@@ -0,0 +1,6 @@
+﻿namespace lab7.Models;
+
+public class User
+{
+    
+}
